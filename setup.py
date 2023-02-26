7305bbd4d907b92f9bfbc2611e331157001cc9d9
Noam Bernstein <noam.bernstein@nrl.navy.mil>
2023-02-26 14:09:11 -0500

    Remove sklearn as a dependency of HAL itself, but install it for github CI pytest

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name="ACEHAL",
     version="0.0.1",
     packages=setuptools.find_packages(exclude=["tests"]),
-    install_requires=["ase", "julia", "numpy", "matplotlib", "pandas", "scipy", "scikit-learn", "timeout_decorator", "optuna", "click>=8.0"],
+    install_requires=["ase", "julia", "numpy", "matplotlib", "pandas", "scipy", "timeout_decorator", "optuna", "click>=8.0"],
     # entry_points="""
     # [console_scripts]
     # tsa=trajstruct.cli:cli
