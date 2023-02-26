1b91e40a409cd75cf087c1f6bd891a575452fcce
Noam Bernstein <noam.bernstein@nrl.navy.mil>
2023-02-26 11:34:11 -0500

    Add github CI auto-running of pytest

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name="ACEHAL",
     version="0.0.1",
     packages=setuptools.find_packages(exclude=["tests"]),
-    install_requires=["ase", "julia", "click>=8.0", "numpy", "scipy", "timeout_decorator", "optuna"],
+    install_requires=["ase", "julia", "numpy", "matplotlib", "scipy", "scikit-learn", "timeout_decorator", "optuna", "click>=8.0"],
     # entry_points="""
     # [console_scripts]
     # tsa=trajstruct.cli:cli
