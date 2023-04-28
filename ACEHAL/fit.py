31fe571e654c2077c428188200d247ace1fec898
JPDarby <jamesdarby@live.co.uk>
2023-04-28 13:24:52 +0100

    use variances to get non-zero features

--- ACEHAL/fit.py
+++ ACEHAL/fit.py
@@ -309,7 +309,7 @@ def do_fit(Psi, Y, B, E0s, solver, n_committee=8, basis_normalization=None, pot_
             solver.reset_threshold(threshold)
             coef_t = np.array(solver.coef_)
             residuals_t = Psi@coef_t-Y
-            K = np.sum(abs(coef_t) > 1e-15)
+            K = np.sum(solver.var_c_ > (solver.var_c_min * solver.threshold))
             BIC = n * np.log(np.mean(residuals_t ** 2)) + K * np.log(n)
             history.append([threshold, BIC, K])
         history = sorted(history, key = lambda x: x[1])
@@ -350,7 +350,7 @@ def do_fit(Psi, Y, B, E0s, solver, n_committee=8, basis_normalization=None, pot_
                 included_c = solver.lambda_ < solver.threshold_lambda
             #BRM from Noam and Chucks
             else:
-                included_c = abs(solver.coef_) > 1e-15
+                included_c = solver.var_c_ > (solver.var_c_min * solver.threshold)
 
             assert sigma.shape[0] == sum(included_c)
 
